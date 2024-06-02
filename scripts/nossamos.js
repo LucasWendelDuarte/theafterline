27628cd36a6c44cb8e4539985969183c8fd4502d
Lucas Wendel Duarte <contatolucaswendel@gmail.com>
2024-06-02 16:27:29 -0300

    atualizacao LOGIN / REGISTRO

--- scripts/nossamos.js
+++ scripts/nossamos.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var typed = new Typed('#typed', {
-        strings: ["Designer", "Developer", "Freelancer", "Photographer"],
+        strings: ["Project Createon", "The Afterline"],
         typeSpeed: 100,
         backSpeed: 50,
-        loop: true
+        loop: false
     });
 });
