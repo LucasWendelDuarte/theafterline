4e26e6ae31ae5069447434cd73d556a4b4f6b3fa
LUCAS WENDEL DUARTE <contatolucaswendel@gmail.com>
2024-06-06 09:02:47 -0300

    Finalizaçao da header/index.html

--- scripts/nossamos.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var typed = new Typed('#typed', {
-        strings: ["Project Createon", "The Afterline"],
-        typeSpeed: 100,
-        backSpeed: 50,
-        loop: false
-    });
-});
